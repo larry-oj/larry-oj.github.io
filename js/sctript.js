a8969bff285e5167d942349614d56f2a2d3580f0
Larry <66905431+larry-oj@users.noreply.github.com>
2024-01-06 16:22:22 +0200

    temp hosting

---
+++ js/sctript.js
@@ -0,0 +1,9 @@
+const phone = document.getElementById('phone');
+phone.getElementsByTagName('a')[0].onclick = function () {
+    phone.innerHTML = '<a href="tel:+380507706434">+380 (50) 770-64-34</a>';
+}
+
+const email = document.getElementById('email');
+email.getElementsByTagName('a')[0].onclick = function () {
+    email.innerHTML = '<a href="mailto:me@larryoj.tk">me@larryoj.tk</a>';
+}
